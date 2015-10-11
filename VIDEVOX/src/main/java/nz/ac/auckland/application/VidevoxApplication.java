6c8ab2d80f11d2cfb817043b61090695eb8e247e
FCDevman <fac7@njit.edu>
2015-10-12 02:56:12 +1300

    Added DarkTheme.css edited layout sizes

--- VIDEVOX/src/main/java/nz/ac/auckland/application/VidevoxApplication.java
+++ VIDEVOX/src/main/java/nz/ac/auckland/application/VidevoxApplication.java
@@ -10,8 +10,15 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import nz.ac.auckland.model.VidevoxModel;
+import nz.ac.auckland.view.PlayerViewController;
+import nz.ac.auckland.view.RootLayoutController;
 import nz.ac.auckland.view.VIDEVOXController;
 
+/**
+ *
+ * @author Fraser
+ *
+ */
 public class VidevoxApplication extends Application {
 
 	/**
@@ -26,16 +33,31 @@ public class VidevoxApplication extends Application {
 	 * The shell layout with menu bars etc.
 	 */
 	private BorderPane _rootLayout;
+	/**
+	 *
+	 */
+	private RootLayoutController _controller;
 
+	/**
+	 *
+	 */
 	public void showPlayerView() {
 		try {
 			// Load PlayerView
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(VidevoxApplication.class.getResource("view/PlayerView.fxml"));
+			loader.setLocation(VidevoxApplication.class.getResource("/nz/ac/auckland/view/PlayerView.fxml"));
+			AnchorPane playerView = (AnchorPane) loader.load();
 
 			// Place it inside the root layout
-			AnchorPane playerView = (AnchorPane) loader.load();
 			_rootLayout.setCenter(playerView);
+
+			// Give the controller class the references it wants
+			PlayerViewController controller = loader.getController();
+			controller.setMainApp(this);
+			controller.setModel(_model);
+
+			// Set view toggle buttons
+			_controller.setViewToggle(RootLayoutController.PREVIEW);
 		} catch (IOException e) {
 			// At this point, there is not much use trying to recover at this
 			// point
@@ -44,19 +66,30 @@ public class VidevoxApplication extends Application {
 	}
 
 	private void initRootLayout() {
-		// Load RootLayout
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(VidevoxApplication.class.getResource("view/RootLayout.fxml"));
+		try {
+			// Load RootLayout
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(VidevoxApplication.class.getResource("/nz/ac/auckland/view/RootLayout.fxml"));
+			_rootLayout = (BorderPane) loader.load();
 
-		// Show on stage
-		Scene scene = new Scene(_rootLayout);
-		_primaryStage.setScene(scene);
-		_primaryStage.show();
+			// Show on stage
+			Scene scene = new Scene(_rootLayout);
+			_primaryStage.setScene(scene);
+			_primaryStage.show();
 
-		// Give controller access to main app
-		VIDEVOXController controller = loader.getController();
-		controller.setMainApp(this);
-		controller.setModel(_model);
+			// Give controller access to main app
+			_controller = loader.getController();
+			_controller.setMainApp(this);
+			_controller.setModel(_model);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("stopping!");
+		return;
 	}
 
 	@Override
@@ -66,14 +99,23 @@ public class VidevoxApplication extends Application {
 		// Set a title to appear on the window
 		this._primaryStage.setTitle("VIDEVOX - video editor");
 
+		// Create a blank project so that _model is not null
+		_model = new VidevoxModel();
+
 		// Initiate the root layout of the application
 		initRootLayout();
+
+		// Show the player view as default
+		showPlayerView();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	/**
+	 *
+	 */
 	public void findModel() {
 		// Display file chooser
 		// pass file to loadMoedl();
