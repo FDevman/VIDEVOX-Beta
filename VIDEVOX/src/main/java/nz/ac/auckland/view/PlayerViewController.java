f86886c89ee038cf460f186c7f37bc88e8917305
FCDevman <fac7@njit.edu>
2015-10-12 01:04:37 +1300

    Added player view

---
+++ VIDEVOX/src/main/java/nz/ac/auckland/view/PlayerViewController.java
@@ -0,0 +1,38 @@
+package nz.ac.auckland.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.MediaView;
+import nz.ac.auckland.model.VidevoxModel;
+
+/**
+ *
+ * @author Fraser
+ *
+ */
+public class PlayerViewController extends VIDEVOXController {
+
+	@FXML
+	private MediaView _mainPlayerView;
+
+	@FXML
+	private Button _playPauseButton;
+
+	@FXML
+	private Button _skipBackButton;
+
+	@FXML
+	private Button _skipForwardButton;
+
+	@FXML
+	private VBox _scrollBox;
+
+	@Override
+	public void setModel(VidevoxModel model) {
+		super.setModel(model);
+		_model.setVideoView(_mainPlayerView);
+		// Load audio into audio views
+	}
+
+}
