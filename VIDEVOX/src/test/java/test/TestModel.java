32c18b14a69aa50605d4304558ef85651db7611c
FCDevman <fac7@njit.edu>
2015-10-12 03:49:48 +1300

    Added logging dependencies

--- VIDEVOX/src/test/java/test/TestModel.java
+++ VIDEVOX/src/test/java/test/TestModel.java
@@ -1,12 +1,30 @@
 package test;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import nz.ac.auckland.model.VidevoxModel;
 
 public class TestModel {
 
-	@Test
-	public void dummyTest() {
+	private static VidevoxModel _model;
 
+	@Before
+	public static void beforeTest() {
+		// Reset model to empty model
+		_model = new VidevoxModel();
+	}
+
+	@Test
+	public void testToStringBasic() {
+		String str = _model.toString();
+		VidevoxModel newModel = new VidevoxModel(str);
+		if (!_model.equals(newModel)) {
+
+			fail("");
+		}
 	}
 
 }
