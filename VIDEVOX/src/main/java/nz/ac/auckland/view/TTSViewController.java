b6c74142d3345de44a5eaa60e425df386a99ce63
FCDevman <fac7@njit.edu>
2015-10-24 00:58:33 +1300

    Created tts view and controller tags

--- VIDEVOX/src/main/java/nz/ac/auckland/view/TTSViewController.java
+++ VIDEVOX/src/main/java/nz/ac/auckland/view/TTSViewController.java
@@ -1,11 +1,51 @@
 package nz.ac.auckland.view;
 
+import java.awt.TextArea;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import nz.ac.auckland.application.VidevoxApplication;
 
 public class TTSViewController extends VIDEVOXController {
 
-	
-	
+	@FXML
+	private HBox _buttonBox;
+
+	@FXML
+	private TextField _nameField;
+
+	@FXML
+	private TextArea _content;
+
+	@FXML
+	private TextField _offset;
+
+	@FXML
+	private void validateTime() {
+
+	}
+
+	@FXML
+	private void cancel() {
+
+	}
+
+	@FXML
+	private void preview() {
+
+	}
+
+	@FXML
+	private void save() {
+
+	}
+
+	@FXML
+	private void use() {
+
+	}
+
 	@Override
 	public void setMainApp(VidevoxApplication app) {
 		super.setMainApp(app);
