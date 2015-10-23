9c055bc3ce9ac36bf06bfc9abc0df5e375d47da4
FCDevman <fac7@njit.edu>
2015-10-24 00:06:26 +1300

    Created tts view fxml and controller

---
+++ VIDEVOX/src/main/java/nz/ac/auckland/view/TTSViewController.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.view;
+
+import nz.ac.auckland.application.VidevoxApplication;
+
+public class TTSViewController extends VIDEVOXController {
+
+	
+	
+	@Override
+	public void setMainApp(VidevoxApplication app) {
+		super.setMainApp(app);
+		// Anything else?
+	}
+
+}
